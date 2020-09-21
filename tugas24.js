46e505130f817720de5d6dc6c910158865086064
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-21 13:30:37 +0700

    Add files via upload

---
+++ tugas24.js
@@ -0,0 +1,6 @@
+  
+var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66]
+
+console.log("Sebelumnya : ", angka.toString())
+console.log("Ascending : ", angka.sort().toString())
+console.log("Descending : ", angka.reverse().toString())
